20a0ae81837479af4688bedefe4e0623b7b2c149
Francisco Sepulveda <franciscosepul96@gmail.com>
2020-05-29 20:03:55 -0400

    Se repara home

--- src/main/java/Ferme/Dao/ProductoDao.java
+++ src/main/java/Ferme/Dao/ProductoDao.java
@@ -10,42 +10,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductoDao implements Crud {
+
     Producto prod = new Producto();
     ResultSet rs = null;
     Connection con = null;
     CallableStatement call = null;
     String query = "";
-    
+
+    int cont = 0;
+
     @Override
     public List<Producto> Listar() {
-        
-        List<Producto> lis = new ArrayList();
-        query="select * from producto";
+
+        ArrayList list = new ArrayList();
+        query = "select * from producto";
         try {
             con = Conexion.getConexion();
             call = con.prepareCall(query);
-            rs=call.executeQuery();
+            rs = call.executeQuery();
             while (rs.next()) {
-                prod.setNombre(rs.getString("nombre"));
-                prod.setPrecioUni(rs.getInt("preciouni"));
-                prod.setRutaDoc(rs.getString("rutadoc"));
-                prod.setStock(rs.getInt("stock"));
-                lis.add(prod);
-                System.out.println("loco aca empieza el producto dao");
-                System.out.println(lis.toString());
-                System.out.println("aca termina el producto dao ");
+                System.out.println(cont + "este es el contador");
+                Producto produc = new Producto();
+                produc.setNombre(rs.getString("nombre"));
+                produc.setPrecioUni(rs.getInt("preciouni"));
+                produc.setRutaDoc(rs.getString("rutadoc"));
+                produc.setStock(rs.getInt("stock"));
+                list.add(cont, produc);
+                cont++;
             }
         } catch (SQLException e) {
-            System.out.println("error producto dao "+e.getMessage());
+            System.out.println("error producto dao " + e.getMessage());
         }
-        return lis;
+        return list;
     }
 
     @Override
     public boolean desabilitar(String rut, String dv) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
-    
+
 }
